b67644bc632585f159c9e239a848e50745c8eb83
Deepak Kumar <deepakkumarnd@gmail.com>
2026-09-02 13:20:36 +0530

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 rsync -av --delete blog/public/ docs/
 cp CNAME docs/
+git add docs/
+today=$(date)
+echo "Publish new update on $today"
+git commit -m "Publish new update on $today"
+git push origin master
+
